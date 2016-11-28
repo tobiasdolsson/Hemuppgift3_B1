1a545beb21d1971990c64040de502d6859a0e8a7
tobiasdolsson <tobias.d.olsson@hotmail.com>
2016-11-28 21:29:21 +0100

    Assignment done, might need some clean up

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,57 @@
 package main;
 
+import java.math.BigInteger;
+
 public class Main {
 	
 	public static void main (String args[]){
-		int p, q, n, r, g;
+		
+		Paillier p = new Paillier(1117,1471,"652534095028","votes.txt");
+		p.extractVotes();
+		BigInteger test = p.numberOfVotes();
+		p.calculateMuAndLambda();
+		p.decryptVotes(test);
+		
+/*		BigInteger p, q, n, r, g, ns, test;
+		BigInteger c1,c2,c3, c;
+		BigInteger my, lambda, m;
+		BigInteger temp1, temp2;
+		BigInteger lambda2;
+		g = new BigInteger("867");
+		p = new BigInteger("5");
+		q = new BigInteger("7");
+		n = p.multiply(q);
+		ns = n.multiply(n);
+		c1 = new BigInteger("929");
+		c2 = new BigInteger("296");
+		c3 = new BigInteger("428");
+		
+		
+		temp1 = p.subtract(BigInteger.valueOf(1));
+		temp2 = q.subtract(BigInteger.valueOf(1));
+		lambda = BigInteger.valueOf(lcm(temp1.longValue(),temp2.longValue()));
+		
+		my = g.modPow(lambda, ns);
+		my = my.subtract(BigInteger.valueOf(1));
+		my = my.divide(n);
+		my = my.modInverse(n);
+		
+		
+		c = c1.multiply(c2).multiply(c3);
+		c = c.mod(ns);
+		
+		//Decrypt
+		m = c.modPow(lambda, ns);
+	
+		m = m.subtract(BigInteger.valueOf(1));
+		m = m.divide(n);
+		
+		m = m.multiply(my).mod(n);
+		m = m.subtract(n);
+		System.out.println(m);*/
+		
 	}
+	
+
 
 }
--- src/main/Paillier.java
+++ src/main/Paillier.java
@@ -1,9 +1,102 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Paillier {
 	
-	public Paillier(int p, int q){
+	private BigInteger p, q, n, ns, m, g, mu, lambda;
+	private String filename;
+	private ArrayList<BigInteger> encryptedVotes;
+	
+	public Paillier(int p, int q, String g, String filename){
+		
+		this.p = BigInteger.valueOf(p);
+		this.q = BigInteger.valueOf(q);
+		this.g = new BigInteger(g);
+		this.filename = filename;
+		encryptedVotes = new ArrayList<BigInteger>();
+		n = this.p.multiply(this.q);
+		ns = n.multiply(n);
 		
 	}
+	
+	public ArrayList<BigInteger> extractVotes(){
+		Scanner inFile = null;
+		String votes;
+		try {
+			inFile = new Scanner(new FileReader(filename));
+
+		} catch (FileNotFoundException e) {
+			System.out.println();
+			e.printStackTrace();
+		}
+
+		while (inFile.hasNext()) {
+			votes = inFile.next();
+			//System.out.println(votes);
+			encryptedVotes.add(new BigInteger(votes));
+		}
+		
+		return encryptedVotes;
+	}
+	
+	public BigInteger numberOfVotes(){
+		
+		BigInteger c = new BigInteger("1");
+		for(int i=0; i<encryptedVotes.size(); i++){
+			c = c.multiply(encryptedVotes.get(i));
+		}
+		c = c.mod(ns);
+		System.out.println("nbrvotes: "+c);
+		return c;
+	}
+	
+	public void calculateMuAndLambda(){
+		BigInteger temp1, temp2;
+		temp1 = p.subtract(BigInteger.valueOf(1));
+		temp2 = q.subtract(BigInteger.valueOf(1));
+		lambda = BigInteger.valueOf(lcm(temp1.longValue(),temp2.longValue()));
+		mu = g.modPow(lambda, ns);
+		mu = mu.subtract(BigInteger.valueOf(1));
+		mu = mu.divide(n);
+		mu = mu.modInverse(n);
+	
+	}
+	
+	
+	public BigInteger decryptVotes(BigInteger c){
+		BigInteger size = BigInteger.valueOf(encryptedVotes.size());
+		m = c.modPow(lambda, ns);		
+		m = m.subtract(BigInteger.valueOf(1)).divide(n);
+		//m = m.divide(n);		
+		m = m.multiply(mu).mod(n);
+		//m = m.subtract(n);
+		System.out.println("vtot: "+m);
+		if(m.compareTo(size)>0){
+			System.out.println("Vote result: "+m.subtract(n));
+		}
+		
+		return m;
+	}
+	
+	private static long gcd(long a, long b){
+	    while (b > 0)
+	    {
+	        long temp = b;
+	        b = a % b; 
+	        a = temp;
+	    }
+	    return a;
+	}
+	
+	private static long lcm(long a, long b){
+	    return a * (b / gcd(a, b));
+	}
+	
 
 }
